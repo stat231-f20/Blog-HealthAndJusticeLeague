cb16efa0793f4733f5e174362a564940d41a4b94
mysubb <msubbanna22@amherst.edu>
2020-11-12 10:54:41 -0500

    added nat disast var input

--- shiny/question3/app.R
+++ shiny/question3/app.R
@@ -14,13 +14,19 @@ infmatmortdata <- read_csv(paste0(path_in,"/wrangled_infmatmortline.csv"))
 natdisastdata <- read_csv(paste0(path_in,"/wrangled_climateq3.csv"))
 
 country_choices <- (infmatmortdata %>%
-                      count(Country))$Country
-country_choice_names <- unique(infmatmortdata$Country)
+                      count(isocode))$isocode
+country_choice_names <- unique(infmatmortdata$isocode)
 names(country_choices) <- country_choice_names
 
 mort_choices_values = names(infmatmortdata)[4:5]
 mort_choices_names <- c("Net Change in Infant Mortality", "Net Change in Maternal Mortality")
-names(mort_choices) <- mort_choices_names
+names(mort_choices_values) <- mort_choices_names
+
+disastfreq_choices_values = names(natdisastdata)[4:10]
+disastfreq_choices_names <- c("All Disasters", "Storms", "Wildfires"
+                              , "Floods", "Droughts", "Landslides"
+                              , "Extreme Temperature Events")
+names(disastfreq_choices_values) <- disastfreq_choices_names
 
 # ui 
 ui <- fluidPage(
@@ -43,11 +49,20 @@ ui <- fluidPage(
 
       ),
       
-      selectInput(inputId = "y"
+      selectInput(inputId = "mort"
+                  , label = "Choose a predictor variable of interest:"
+                  , choices = mort_choices_values
+                  , selected = infmatmortdata$NetChange_Infant
+      ),
+      
+      selectInput(inputId = "disastfreq"
                   , label = "Choose a predictor variable of interest:"
                   , choices = mort_choices_values
                   , selected = infmatmortdata$NetChange_Infant)
+      
     ),
+    
+    
     
     mainPanel(
       
@@ -68,7 +83,7 @@ server <- function(input,output){
   
 
   output$bar1 <- renderPlot({
-    ggplot(data = use_data1_q3(), aes_string(x = "Year", y = input$y)) +
+    ggplot(data = use_data1_q3(), aes_string(x = "Year", y = input$mort)) +
       geom_bar(position = "dodge", stat = "identity", aes_string(fill = "Country")) +
       labs(x = "Year", y = "Net Change in Mortality Per Month"
            , title = "Infant and Maternal Mortality Worldwide from 1980-2018") +
@@ -78,8 +93,8 @@ server <- function(input,output){
   })
   
   output$bar2 <- renderPlot({
-    ggplot(data = use_data1_q3(), aes_string(x = "Year", y = input$y)) +
-      geom_bar(position = "dodge", stat = "identity", aes_string(fill = "Country")) +
+    ggplot(data = use_data1_q3(), aes_string(x = "Year", y = input$disastfreq)) +
+      geom_bar(position = "dodge", stat = "identity", aes_string(fill = "isocode")) +
       labs(x = "Year", y = "Net Change in Mortality Per Month"
            , title = "Infant and Maternal Mortality Worldwide from 1980-2018") +
       theme(legend.position="bottom", plot.title = element_text(hjust = 0.5)) + 

b60c7d8940bfd66b6de788d9279d10d19b78a836
LillianYKim <yeskim20@amherst.edu>
2020-11-14 23:25:20 +0900

    minor changes

--- shiny/app.R
+++ shiny/app.R
@@ -7,6 +7,7 @@ library(janitor)
 library(countrycode)
 library(shiny)
 library(shinythemes)
+library(plotly)
 
 # calling in the datasets
 my_path <- "C:/Users/Yesuel Kim/Documents/Git/Blog-HealthAndJusticeLeague"
@@ -20,23 +21,12 @@ natdis_leaflet <- maps::map("world", fill = TRUE, plot = FALSE, wrap=c(-180,180)
 # and country name used in maps package
 data(iso3166)
 
-#####
-# ignore this part
-# natdis_leaflet$country <- str_extract(natdis_leaflet$names, "[^:]+")
-# natdis_leaflet$code <- natdis_leaflet$country %>% countrycode(origin = "country.name", destination = "iso3c")
-# natdis_leaflet$freq <- natdis$occurrence[match(natdis_leaflet$code, natdis_react$countrycode)]
-# natdis_leaflet$deaths <- natdis$deaths[match(natdis_leaflet$code, natdis_react$countrycode)]
-# natdis_leaflet$injured <- natdis$injured[match(natdis_leaflet$code, natdis_react$countrycode)]
-# natdis_leaflet$homeless <- natdis$homeless[match(natdis_leaflet$code, natdis_react$countrycode)]
-# natdis_leaflet$total <- natdis$total[match(natdis_leaflet$code, natdis_react$countrycode)]
-######
-
 # a3 is iso3c country code
 list_countries <- iso3166 %>%
   select(a3, ISOname, mapname)
 
 # HTML Text: background info to display on the side
-text <- sprintf("<strong>%s</strong>%s<br/><br/>%s<br/>%s<br/>%s<br/>%s<br/>%s<br/>",
+text <- sprintf("<strong>%s</strong>%s<br/><br/>%s<br/>%s<br/>%s<br/>%s<br/>%s<br/><br/>",
                 "Source: EM-DAT",
                 a(href="https://www.emdat.be/", " (The International Disaster Database)"),
                 "This app only shows the natural disasters that fulfill the following criteria:",
@@ -45,27 +35,6 @@ text <- sprintf("<strong>%s</strong>%s<br/><br/>%s<br/>%s<br/>%s<br/>%s<br/>%s<b
                 "c) A state of emergency declared; or",
                 "d) International assistance requested")%>% 
   lapply(htmltools::HTML)
-
-
-######
-#ignore this part
-# check if the country code for important/significant countries ended up missing
-# world_missing <- world_code %>%
-#   filter(is.na(code))
-
-# The missing country/regions are: 
-# Ascension Island, Azores, Barbuda, Bonaire, Canary Islands, 
-# Chagos Archipelago, Grenadines, Heard Island, Kosovo, Madeira Islands, 
-# Micronesia, Saba, Saint Martin, Siachen Glacier, Sint Eustatius, 
-# Virgin Islands
-######
-
-######
-# for selectInput drop-down choices(types of natural disasters)
-# relevant <- c("All", "Drought","Storm", "Flood", "Landslide", 
-#               "Wildfire", "Extreme temperature")
-# names(relevant) <- relevant
-######
 
 # for checkboxGroupInput choices
 relevant <- unique(natdis$disaster_type)
@@ -92,42 +61,12 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                   ticks = TRUE,
                   width = "90%"))),
     fluidRow(
-      column(width = 3,
-             text),
-      column(width = 9,
+      column(width = 5,
+            text
+            ),
+      column(width = 7,
            leafletOutput("natdisplot"))
   )
-  
-  ######
-  
-  # sidebarLayout(
-  #   sidebarPanel(
-  #     # year
-  #     sliderInput(inputId = "year",
-  #                 label = "Select the year",
-  #                 min = 1980,
-  #                 max = 2020,
-  #                 value = 2000,
-  #                 sep = "",
-  #                 ticks = TRUE),
-  #     # disaster type
-  #     # selectInput(inputId = "disaster",
-  #     #             label = "Select the disaster type",
-  #     #             choices = relevant,
-  #     #             selected = "All",
-  #     #             selectize = FALSE)
-  #     
-  #     checkboxGroupInput(inputId = "disaster",
-  #                        label = "Select disaster(s)",
-  #                        choices = relevant,
-  #                        selected = relevant,
-  #                        inline = TRUE)
-  #     ),
-  #   
-  #   mainPanel(leafletOutput("natdisplot"))
-  # )
-  
-  ######
 )
 
 
@@ -144,6 +83,7 @@ server <- function(input, output){
       right_join(list_countries, by = c("countrycode" = "a3"))
     
   })
+  
   
   a <- reactive({
     # country name
@@ -180,6 +120,7 @@ server <- function(input, output){
   pal_natdis <- reactive({
     colorBin("YlOrRd", domain = a()$total, bins = bins)
   })
+  
   
   
   output$natdisplot <- renderLeaflet({

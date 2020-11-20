71b4a17c8078ec3019cf7f9402d7882b577b16a2
mysubb <msubbanna22@amherst.edu>
2020-11-20 01:55:58 -0500

    added code with edited axes for graphs

--- shiny/question3/app.R
+++ shiny/question3/app.R
@@ -119,8 +119,10 @@ server <- function(input,output){
     ggplot(data = use_data1_q3(), aes_string(x = "Year", y = input$mort)) +
       geom_bar(position = "dodge", stat = "identity", aes_string(fill = "isocode")) +
       labs(x = "Year"
-           , y = case_when(input$mort == "NetChange_Infant" ~ "Net Change in Mortality Per Month \n (deaths per 1,000 live births)"
-                           , input$mort == "NetChange_Maternal" ~ "Net Change in Mortality Per Month \n (deaths per 100,000 live births)")
+           , y = case_when(input$mort == "NetChange_Infant" 
+                           ~ "Net Change in Infant Mortality Per Year \n (deaths per 1,000 live births)"
+                           , input$mort == "NetChange_Maternal" 
+                           ~ "Net Change in Maternal Mortality Per Year \n (deaths per 100,000 live births)")
            , title = "Infant and Maternal Mortality Worldwide from 1980-2018") + 
       theme_bw() + 
       theme(legend.position="bottom", plot.title = element_text(hjust = 0.5)
